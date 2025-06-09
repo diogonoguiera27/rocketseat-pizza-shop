d22ff13015d175346852f3c9a7bbdd320aadceba
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-06-09 11:11:37 -0300

    React Hook Form – form validation setup

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -4,8 +4,23 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
+import {useForm} from "react-hook-form"
+import {z} from "zod";
 
-export function SignIn() {
+
+const signInForm = z.object({
+  email: z.string().email("E-mail inválido"),})
+
+type SignInForm = z.infer<typeof signInForm>;
+  export function SignIn() {
+
+  const { register, handleSubmit , formState:{isSubmitting}} = useForm<SignInForm >();
+
+  async function handleSignIn(data:SignInForm ){
+    console.log(data);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
+
   return (
     <>
       <Helmet title="login" />
@@ -20,13 +35,15 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form  onSubmit={handleSubmit(handleSignIn)}  className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email"> Seu e-mail </Label>
-              <Input id="email" type="email"/>
+              <Input id="email" type="email" {...register('email')}/>
             </div>
 
-            <Button  className="w-full" type="submit"> Acessar Painel </Button>
+            <Button disabled={isSubmitting}  className="w-full" type="submit"> 
+              Acessar Painel 
+            </Button>
           </form>
         </div>
       </div>
