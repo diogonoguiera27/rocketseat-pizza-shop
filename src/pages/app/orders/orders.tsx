3d98d95a0e4a4acbc6835aaa8f2fd7024f7c8924
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-06-10 10:07:45 -0300

    created order listing interface

---
+++ src/pages/app/orders/orders.tsx
@@ -0,0 +1,88 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { ArrowBigRight, Search, X } from "lucide-react";
+import { Helmet } from "react-helmet-async";
+
+export function Orders() {
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-sm font-bold tracking-light">Pedidos</h1>
+      </div>
+      <div className="space-y-2.5">
+        <form className="flex items-center gap-2">
+          <span className="text-sm font-semibold">Filtros</span>
+          <Input placeholder="Nome do Cliente" className="h-8 w-[320px]" />
+        </form>
+
+        <div className="border rounded-md">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="W-[64px]"></TableHead>
+                <TableHead className="W-[140px]">Indentificador</TableHead>
+                <TableHead className="W-[180px]">Realizado ha</TableHead>
+                <TableHead className="W-[140px]">Status</TableHead>
+                <TableHead>Cliente</TableHead>
+                <TableHead className="W-[140px]">Total do Pedido</TableHead>
+                <TableHead className="W-[164px]"></TableHead>
+                <TableHead className="W-[132px]"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Array.from({ length: 10 }).map((_, i) => {
+                return (
+                  <TableRow key={i}>
+                    <TableCell>
+                      <Button variant="outline" size="xs">
+                        <Search className="h-3 w-3" />
+                        <span className="sr-only">Detalhes do Pedido</span>
+                      </Button>
+                    </TableCell>
+                    <TableCell className="font-mono text-xs font-medium">
+                      321322231dsdasd412
+                    </TableCell>
+                    <TableCell className="text-muted-foreground">
+                      há 15 minutos
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex items-center gap-2">
+                        <span className="h-2 w-2 rounded-full bg-slate-400" />
+                        <span className="font-medium text-muted-foreground">
+                          Pendente
+                        </span>
+                      </div>
+                    </TableCell>
+                    <TableCell className="font-medium">Diogo Alves</TableCell>
+                    <TableCell className="font-medium">R$ 149,90</TableCell>
+                    <TableCell>
+                      <Button variant="outline" size="xs">
+                        <ArrowBigRight className="mr-2 h-3 w-3" />
+                        Aprovar
+                      </Button>
+                    </TableCell>
+                    <TableCell>
+                      <Button variant="ghost" size="xs">
+                        <X className="mr-2 h-3 w-3" />
+                        Cancelar
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </>
+  );
+}
