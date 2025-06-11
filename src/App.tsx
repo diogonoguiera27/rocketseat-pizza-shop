e26aa9b7594f45c233d776e890803f88b1adf719
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-06-11 13:49:12 -0300

    configure API client for frontend-backend communication

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
   return (
@@ -11,7 +13,9 @@ export function App() {
       <ThemeProvider storageKey="pizza-shop-theme" defaultTheme="dark">
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   );
