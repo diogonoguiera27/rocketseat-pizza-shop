605e06fe96295b4462a5774bc3e640d80e28fa3a
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-06-12 14:38:00 -0300

    metrics on dashboard

--- src/pages/app/dashboard/month-canceled-orders-amount.tsx
+++ src/pages/app/dashboard/month-canceled-orders-amount.tsx
@@ -1,7 +1,13 @@
+import { getMonthCanceledOrdersAmount } from "@/api/get-month-canceled-orders-amount";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
 import { DollarSign } from "lucide-react";
 
 export function MonthCanceledOrdersAmountCard() {
+  const { data: monthCanceledOrdersAmount } = useQuery({
+    queryFn: getMonthCanceledOrdersAmount,
+    queryKey: ["metrics", "month-canceled-orders-amount"],
+  });
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -11,11 +17,30 @@ export function MonthCanceledOrdersAmountCard() {
         <DollarSign className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">R$ 32</span>
-        <p className="text-xs text-muted-foreground">
-          <span className=" text-emerald-500 dark:text-emerald-400">-2%</span>{" "}
-          em relação ao mes passado
-        </p>
+        {monthCanceledOrdersAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {monthCanceledOrdersAmount.amount.toLocaleString("pt-BR")}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {monthCanceledOrdersAmount.diffFromLastMonth < 0 ? (
+                <>
+                  <span className=" text-emerald-500 dark:text-rose-400">
+                    {monthCanceledOrdersAmount.diffFromLastMonth}%
+                  </span>{" "}
+                  em relação ao mes passado
+                </>
+              ) : (
+                <>
+                  <span className=" text-rose-500 dark:text-rose-400">
+                    +{monthCanceledOrdersAmount.diffFromLastMonth}%
+                  </span>{" "}
+                  em relação ao mes passado
+                </>
+              )}
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   );
