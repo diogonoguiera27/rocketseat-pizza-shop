6cf2061d652d6944a59e0c2ee2f56f153f7609e3
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-06-12 12:58:29 -0300

    cancel order

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -4,9 +4,12 @@ import { TableCell, TableRow } from "@/components/ui/table";
 import { ArrowBigRight, Search, X } from "lucide-react";
 import { OrderDetails } from "./order-details";
 import { OrderStatus } from "./order-status";
-import { formatDistanceToNow } from 'date-fns'
-import {ptBR} from 'date-fns/locale'
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { cancelOrder } from "@/api/cancel-order";
+import { GetOrdersResponse } from "@/api/get-orders";
 
 export interface OrderTableRowProps {
   order: {
@@ -19,7 +22,34 @@ export interface OrderTableRowProps {
 }
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
-   const [isDetailsOpen,setIsDetailsOpen] = useState(false)
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+  const queryClient = useQueryClient()
+
+  const {mutateAsync: cancelOrderfn} = useMutation({
+    mutationFn:cancelOrder,
+    async onSuccess(_, {orderId}){
+      const ordersListCache = queryClient.getQueriesData<GetOrdersResponse>({
+        queryKey:['orders']
+      })
+
+      ordersListCache.forEach(([cacheKey,cacheData]) =>{
+        if (!cacheData){
+          return
+        }
+
+        queryClient.setQueryData<GetOrdersResponse>(cacheKey,{
+          ...cacheData,
+          orders: cacheData.orders.map(order =>{
+            if (order.orderId === orderId){
+              return {...order,status: 'canceled'}
+            }
+
+            return order
+          }),
+        })
+      })
+    },
+  })
   return (
     <TableRow>
       <TableCell>
@@ -31,16 +61,16 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
             </Button>
           </DialogTrigger>
 
-          <OrderDetails  open={isDetailsOpen} orderId={order.orderId}/>
+          <OrderDetails open={isDetailsOpen} orderId={order.orderId} />
         </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
         {order.orderId}
       </TableCell>
       <TableCell className="text-muted-foreground">
-        {formatDistanceToNow(order.createdAt,{
-          locale:ptBR,
-          addSuffix:true
+        {formatDistanceToNow(order.createdAt, {
+          locale: ptBR,
+          addSuffix: true,
         })}
       </TableCell>
       <TableCell>
@@ -48,7 +78,7 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
       </TableCell>
       <TableCell className="font-medium">{order.customerName}</TableCell>
       <TableCell className="font-medium">
-        {(order.total/100).toLocaleString("pt-BR", {
+        {(order.total / 100).toLocaleString("pt-BR", {
           style: "currency",
           currency: "BRL",
         })}
@@ -60,7 +90,12 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
         </Button>
       </TableCell>
       <TableCell>
-        <Button variant="ghost" size="xs">
+        <Button
+          disabled={!["pending", "processing"].includes(order.status)}
+          onClick={()=> cancelOrderfn({orderId:order.orderId})}
+          variant="ghost"
+          size="xs"
+        >
           <X className="mr-2 h-3 w-3" />
           Cancelar
         </Button>
