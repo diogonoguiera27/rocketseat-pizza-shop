30560449369e386f80c5e65937f6bd87b6277b7c
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-06-12 18:11:00 -0300

    loading state on order detail

--- src/pages/app/orders/order-details.tsx
+++ src/pages/app/orders/order-details.tsx
@@ -18,6 +18,7 @@ import { useQuery } from "@tanstack/react-query";
 import { OrderStatus } from "../../../components/order-status";
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { OrderDetailsSkeleton } from "./order-details-skeleton";
 
 export interface OrderDetailsProps {
   orderId: string;
@@ -38,7 +39,7 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
         <DialogDescription>Detalhes do pedido</DialogDescription>
       </DialogHeader>
 
-      {order && (
+      {order ? (
         <div>
           <Table>
             <TableBody>
@@ -133,6 +134,8 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
             </TableFooter>
           </Table>
         </div>
+      ):(
+        <OrderDetailsSkeleton/>
       )}
     </DialogContent>
   );
